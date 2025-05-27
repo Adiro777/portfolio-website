48f117292b380e058f355f0acaa78872a2cb5b5d
Adiro777 <112520123+Adiro777@users.noreply.github.com>
2025-05-26 19:47:18 -0700

    Routing progress

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import Resume from './Resume';
 
 function App() {
   return (
-   <Navbar />
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/resume">
+          <Resume />
+        </Route>
+
+      </Switch>
+     
+    </Router>
   );
 }
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <a className="NavItem">Home</a>
-      <a className="NavItem">Resume</a>
+      <a className="NavItem" href='/'>Home</a>
+      <a className="NavItem" href='/resume'>Resume</a>
       <a className="NavItem">Contact</a>
     </div>
   )
